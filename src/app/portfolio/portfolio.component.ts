7fe2a22c5c39797d0cef2f38d4b4a67eb4112cf1
aalake <alake011@fiu.edu>
2021-02-04 11:32:01 -0500

    Developed projects in portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -19,24 +19,21 @@ export class PortfolioComponent implements OnInit {
    description: `A web application built under the PPD (Pinecrest Police
     Department) to be used by civilians and police departments, to communicate
     information of crimes and suspicious activities`, 
-  //  techStack:['Angular','HTML5','Bootstrap','Angular Material UI', 'Java Spring Boot',
-  //   'RESTFUL API', 'SQL(MSSQL)', 'Git(Github)', 'npm', 'Agile(Scrum)' ],
-    techStack:['Angular, HTML5, Bootstrap, Angular Material UI','Java Spring Boot, RESTFUL API, SQL(MSSQL)',
-    'Git(Github), npm, VS Code, Agile(Scrum) '],
+    techStack:['Angular', 'HTML5', 'CSS3', 'Material UI','Java Spring', 'MSSQL'],
    contributions:'Frontend (Angular, HTML5, CSS3, Bootstrap, Angular Material UI)'},
 
   { title:'Portfolio Site', 
   description: `My site is used as another avenue for people to get to know me.
   It has communication features,background info & personal interest in one place`, 
-  techStack:['Angular(v.10)','HTML5','CSS3','Material UI','Bootstrap', 
-  'SQL(AWS MySQL RDBS)', 'Node.js','Express.js','Git(Github)','npm'],
+  techStack:['Angular','HTML5','CSS3','Material UI','Bootstrap', 
+  'MySQL', 'Node','Express'],
   contributions:'Designed and built site'},
 
   {title:'BookBench', 
   description: `A small web application project, built in collaboration with 
   a software engineering team, for university requirement. It's main purpose was as
   an online book library with beautiful UI features and the ability to make purchases`, 
-  techStack:['PHP','HTML5','CSS3','SQL(MySQL)','JavaScript','Apache HTTP Server','Agile(Scrum)'], 
+  techStack:['PHP','HTML5','CSS3','MySQL','JavaScript','Apache'], 
   contributions:'Used all technologies in the stack, to develop on the frontend and backend'}
 ]  
   constructor() {  }
