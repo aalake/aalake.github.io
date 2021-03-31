4cd7367b60186ceb42116b1a86081b946c968a8f
aalake <45490749+aalake@users.noreply.github.com>
2021-03-31 15:09:07 -0400

    Updated config file with secrets

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "rds-mysql-portfoliodb.cw0iyle6mgai.us-east-2.rds.amazonaws.com",
-    USER: "admin",
-    PASSWORD: "theadminpass8996",
-    DB: "portfoliodb"
+    HOST: CREDHOST,
+    USER: CREDUSER,
+    PASSWORD: CREDPASS,
+    DB: CREDDB
 }
